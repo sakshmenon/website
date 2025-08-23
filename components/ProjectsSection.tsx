2eb70f0df5d7fae9362f439ed678fa92b1e44a86
Saksh Menon <98868994+sakshmenon@users.noreply.github.com>
2025-08-23 16:59:56 -0400

    Referencing hosted images

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -26,7 +26,7 @@ export function ProjectsSection() {
     {
       title: 'SafEE - Personalized Video Player',
       description: 'A video streaming app that warns users for sudden spikes in audio/brightness helping those sensitive to such spikes such as epileptic users.',
-      image: '/img/video.png',
+      image: 'https://raw.githubusercontent.com/sakshmenon/website/refs/heads/main/video.png',
       tags: ['HTML', 'JavaScript', 'OpenCV', 'PyAudio', 'PyQt'],
       duration: 'Jan 2025 - Feb 2025'
     }
